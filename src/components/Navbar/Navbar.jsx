327ae4b9c9ca577fa0ecde89bf7df00888c6df1e
Manmeet <manmeetnagiii@gmail.com>
2024-02-25 13:18:16 +0530

    menu-toggle-added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,32 @@
-import React from 'react'
-import searchIcon from "../../assets/search.svg"
-import './Navbar.scss'
+import React, { useState } from "react";
+import searchIcon from "../../assets/search.svg";
+import "./Navbar.scss";
 
 const Navbar = () => {
+
+  const [menu, setMenu] = useState(true);
+
+
+  
   return (
     <div className="navbar-container">
-        <div className="search">
-            <img src={searchIcon} alt="" />
-            <input type="text" placeholder='Search'/>
-        </div>
-        <div className="navbar-links">
-            <a href="/">Categories</a>
-            <a href="/">Website Builders</a>
-            <a href="/">Today's deals</a>
-        </div>
-   </div>
-  )
-}
+      <div className="search">
+        <img src={searchIcon} alt="" />
+        <input type="text" placeholder="Search" />
+      </div>
+      <div className="navbar-links">
+        <a href="#">Categories</a>
+        <a href="#">Website Builders</a>
+        <a href="#">Today's deals</a>
+      </div>
+      <div className={(menu? 'mob-menu' : 'hidden')}>
+        <a href="#">Categories</a>
+        <a href="#">Website Builders</a>
+        <a href="#">Today's deals</a>
+      </div>
+      <span onClick={() => setMenu(!menu)} class="material-symbols-outlined">menu</span>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
